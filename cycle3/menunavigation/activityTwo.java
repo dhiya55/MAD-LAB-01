5e307bdef3d41dd0cbff2f60b3b7839bf1f57d00
dhiya55 <fathimadhiya5515@gmail.com>
2025-09-16 15:11:45 +0530

    Add files via upload

---
+++ cycle3/menunavigation/activityTwo.java
@@ -0,0 +1,14 @@
+package com.example.menunavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
