0bfa05780bf676695e1af7311ece2cdb02a99820
dhiya55 <fathimadhiya5515@gmail.com>
2025-11-03 07:17:55 +0530

    Refactor calculator logic and variable names

--- cycle_1/simple_calculator/MainActivity.java
+++ cycle_1/simple_calculator/MainActivity.java
@@ -5,68 +5,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView textView1;
-    private Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button0;
-    private Button buttonAdd,buttonSub,buttonMul,buttonDiv,buttonDot,buttonEqual;
-    private String currentInput="";
-    private double operand1=0;
-    private String operator="";
+
+    private TextView display;
+    private String input = "";
+    private double firstOperand = 0;
+    private String operator = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView1=findViewById(R.id.text_View1);
+
+        display = findViewById(R.id.text_View1);
     }
-    public void onDigitClick(View view){
-        Button button=(Button) view;
-        currentInput +=button.getText().toString();
+
+    public void onDigitClick(View view) {
+        input += ((Button) view).getText();
         updateDisplay();
     }
-    public void onOperatorClick(View view){
-        if (!currentInput.isEmpty()) {
-            operand1 = Double.parseDouble(currentInput);
+
+    public void onOperatorClick(View view) {
+        if (!input.isEmpty()) {
+            firstOperand = Double.parseDouble(input);
             operator = ((Button) view).getText().toString();
-            currentInput = "";
+            input = "";
         }
     }
-    public void onEqualsClick(View view){
-        if (!currentInput.isEmpty()){
-            double operand2=Double.parseDouble(currentInput);
-            double result=performOperation(operand1,operand2,operator);
-            currentInput=String.valueOf(result);
-            updateDisplay();
+
+    public void onEqualsClick(View view) {
+        if (input.isEmpty()) return;
+
+        double secondOperand = Double.parseDouble(input);
+        double result = 0;
+
+        switch (operator) {
+            case "+": result = firstOperand + secondOperand; break;
+            case "-": result = firstOperand - secondOperand; break;
+            case "*": result = firstOperand * secondOperand; break;
+            case "/": result = (secondOperand != 0) ? firstOperand / secondOperand : Double.NaN; break;
         }
-    }
-    public void onClearClick(View view){
-        currentInput="";
-        operand1=0;
-        operator="";
+
+        input = String.valueOf(result);
         updateDisplay();
     }
-    private double performOperation(double operand1,double operand2,String operator) {
-        switch (operator) {
-            case "+":
 
-                return operand1 + operand2;
-            case "-":
-
-                return operand1 - operand2;
-            case "*":
-
-                return operand1 * operand2;
-            case "/":
-
-                if (operand2 != 0)
-                    return operand1 / operand2;
-                else
-                    return Double.NaN;
-            default:
-                return 0;
-        }
+    public void onClearClick(View view) {
+        input = "";
+        firstOperand = 0;
+        operator = "";
+        updateDisplay();
     }
-    public void updateDisplay(){
-        textView1.setText(currentInput);
 
+    private void updateDisplay() {
+        display.setText(input);
     }
 }
